e24f282b633840165dc15c499e89abe19c47bf33
matrucci <matan.saloniko@gmail.com>
2020-12-19 21:52:44 +0200

    Finished - pre commenting

--- run_main.s
+++ run_main.s
@@ -1,61 +1,75 @@
 	.section	.rodata
-format_s:	.string	"%c"
-format_d:	.string	"%d"
-format_end:	.string	"\0"
+format_s:	.string		" %s"
+format_d:	.string		" %d"
 
-	.text	#The start of the code
+	.text	#Start of the code
 .global	run_main
 	.type	run_main, @function
 
 run_main:
-	pushq	%r12
-	pushq	%r13
-	movq	%rsp, %rbp
-	movq	$format_d, %rdi	#Getting the length
-	subq	$8, %rsp
-	movq	%rsp, %rsi
+	pushq	%r12		#Backing up callee save
+	pushq	%r13		#Backing up callee save
+	pushq	%r14		#Backing up callee save
+	movq	%rsp, %rbp	#Saving stack state
+
+	movq	$format_d, %rdi
+	subq	$544, %rsp	
+	leaq	(%rsp), %rsi
 	movq	$0, %rax
-	call	scanf			#Scan the length
-	addq	$2, (%rsp)
-	leaq	(%rsi, %rsi, 8), %rsp
-	subq	$2, (%rsp)
-	pushq	(%rsi)			#Backing up the length
-	movq	%rsp, %rsi
+	call	scanf		#Getting the first string length
+
+	movq	%rsp, %r12	#Saving the pointer to the first string
+	leaq	1(%rsp), %rsi
 	movq	$format_s, %rdi
 	movq	$0, %rax
-	call	scanf			#Scan the string
-	movq	$format_end, %rsi	#Adding \0 to the end
-	subq	$8, %rsp
-	movq	%rsp, %r12		#First pstring pointer
-	#Getting pstring2
-	movq	$format_d, %rdi	#Getting the length
-	subq	$8, %rsp
-	movq	%rsp, %rsi
+	call	scanf		#Getting the first string
+
+	xor	%r13, %r13
+	movb	(%rsp), %r13b
+	leaq	1(%rsp, %r13), %rsi
+	movb	$0, (%rsi)	#Adding \0
+	
+	leaq	256(%rsp), %rsi
+	movq	$format_d, %rdi
+	movq	%rsi, %r13	#Saving the pointer to the second string
 	movq	$0, %rax
-	call	scanf			#Scan the length
-	addq	$2, (%rsp)
-	leaq	(%rsi, %rsi, 8), %rsp
-	subq	$2, (%rsp)
-	pushq	(%rsi)			#Backing up the length
-	movq	%rsp, %rsi
+	call	scanf		#Getting the second string length
+	
+	leaq	1(%r13), %rsi
 	movq	$format_s, %rdi
 	movq	$0, %rax
-	call	scanf			#Scan the string
-	movq	$format_end, %rsi	#Adding \0 to the end
-	subq	$8, %rsp
-	movq	%rsp, %r13		#Second pstring pointer
-	#Getting the switch option
-	movq	$format_d, %rdi	#Getting the length
-	subq	$16, %rsp
-	movq	%rsp, %rsi
+	call	scanf		#Getting the second string
+
+	xor	%r14, %r14
+	movb	(%r13), %r14b
+	leaq	1(%r13, %r14), %rsi
+	movb	$0, (%rsi)	#Adding \0
+	
+	leaq	2(%r13, %r14), %rsi
+	movq	%rsi, %r14
+	movq	$format_d, %rdi
 	movq	$0, %rax
-	call	scanf			#Scan the length
-	movq	(%rsp), %rdx
-	movq	%r12, %rdi
-	movq	%r13, %rsi
+	call	scanf		#Getting switch code
+
+	xor	%rsi, %rsi
+	xor	%rdx, %rdx
+	#leaq	257(%rsp, %r14), %rsi
+	#movb	(%rsi), %dl
+	
+	#movq	$0, %rax
+	#movq	$format_d, %rdi
+	#movb	(%r14), %sil
+	#call	printf
+	
+	xor	%rdi, %rdi
+	movb	(%r14), %dil
+	movq	%r12, %rsi
+	movq	%r13, %rdx
 	call	run_func
-	movq	(%rbp), %r13		#Getting r13 back. callee
-	addq	$8, %rbp
-	movq	(%rbp), %r12		#Getting r12 back. callee
-	addq	$8, %rbp
+
+	movq	%rbp, %rsp
+	popq	%r14
+	popq	%r13
+	popq	%r12
+	movq	%rsp, %rbp
 	ret
